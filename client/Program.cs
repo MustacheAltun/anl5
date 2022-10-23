e13182e8c78208e3ec682accc4363873ead45c66
MustacheAltun <51758662+MustacheAltun@users.noreply.github.com>
2022-10-23 23:12:20 +0200

    initial

---
+++ client/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using UDP_FTP.Models;
+using UDP_FTP.Error_Handling;
+using static UDP_FTP.Models.Enums;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: add the student number of your group members as a string value. 
+            // string format example: "Jan Jansen 09123456" 
+            // If a group has only one member fill in an empty string for the second student
+            string student_1 = "Mustafa Altun 1034323";
+            string student_2 = "bob 121211";
+
+
+            byte[] buffer = new byte[1000];
+            // byte[] msg = new byte[100];
+            Socket sock;
+            // TODO: Initialise the socket/s as needed from the description of the assignment
+
+            HelloMSG h = new HelloMSG();
+            RequestMSG r = new RequestMSG();
+            DataMSG D = new DataMSG();
+            AckMSG ack = new AckMSG();
+            CloseMSG cls = new CloseMSG();
+
+
+            
+            h.To = "MyServer";
+            h.From = student_1;
+            h.Type = Messages.HELLO;
+            var test = JsonSerializer.Serialize(h);
+
+            byte[] msg = Encoding.ASCII.GetBytes(test);
+
+            // Socket sock;
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ServerEndpoint = new IPEndPoint(ipAddress, 5004);
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+            EndPoint remoteEP = (EndPoint) sender;
+            try {
+                sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                sock.SendTo(msg, msg.Length, SocketFlags.None, ServerEndpoint);
+                int b = sock.ReceiveFrom(buffer, ref remoteEP);
+                HelloMSG data = JsonSerializer.Deserialize<HelloMSG>(Encoding.ASCII.GetString(buffer, 0, b));
+                Console.WriteLine("Server said" + data.ConID);
+                var ConnectionId = data.ConID;
+                sock.Close();
+            } catch {
+                Console.WriteLine("\n Socket Error. Terminating");
+            }
+            // try
+            // {
+            //     // TODO: Instantiate and initialize your socket 
+
+
+            //     // TODO: Send hello mesg
+
+            //     // TODO: Receive and verify a HelloMSG 
+
+
+            //     // TODO: Send the RequestMSG message requesting to download a file name
+
+            //     // TODO: Receive a RequestMSG from remoteEndpoint
+            //     // receive the message and verify if there are no errors
+
+
+            //     // TODO: Check if there are more DataMSG messages to be received 
+            //     // receive the message and verify if there are no errors
+
+            //     // TODO: Send back AckMSG for each received DataMSG 
+
+
+            //     // TODO: Receive close message
+            //     // receive the message and verify if there are no errors
+
+            //     // TODO: confirm close message
+
+            // }
+            // catch
+            // {
+            //     Console.WriteLine("\n Socket Error. Terminating");
+            // }
+
+            // Console.WriteLine("Download Complete!");
+           
+        }
+    }
+}
---
+++ test/clinetUDP/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      var bob = new UDPClient();
+      System.Net.IPAddress ipaddress = System.Net.IPAddress.Parse("127.0.0.1");
+      bob.Initialize(ipaddress,5000);
+      bob.StartMessageLoop();
+    }
+}
+public class UDPClient
+{
+    private Socket _socket;
+    private EndPoint _ep;
+
+    private byte[] _buffer_recv;
+    private ArraySegment<byte> _buffer_recv_segment;
+
+    public void Initialize(IPAddress address, int port)
+    {
+        _buffer_recv = new byte[4096];
+        _buffer_recv_segment = new(_buffer_recv);
+
+        _ep = new IPEndPoint(address, port);
+
+        _socket = new(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        _socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.PacketInformation, true);
+    }
+
+    public void StartMessageLoop()
+    {
+        _ = Task.Run(async () =>
+        {
+            SocketReceiveMessageFromResult res;
+            while (true)
+            {
+                res = await _socket.ReceiveMessageFromAsync(_buffer_recv_segment, SocketFlags.None, _ep);
+            }
+        });
+    }
+
+    public async Task Send(byte[] data)
+    {
+        var s = new ArraySegment<byte>(data);
+        await _socket.SendToAsync(s, SocketFlags.None, _ep);
+    }
+}
